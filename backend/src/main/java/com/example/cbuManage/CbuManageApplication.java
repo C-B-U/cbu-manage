09408a286ba6d5a2728c2a373dd5e55386a512ee
SeongEun <bongfam2@naver.com>
2024-11-07 19:58:59 +0900

    Setting 1. 스프링부트 프로젝트 생성

---
+++ backend/src/main/java/com/example/cbuManage/CbuManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbuManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbuManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbuManageApplication.class, args);
+	}
+
+}
